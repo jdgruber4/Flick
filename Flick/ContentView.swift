1838af98a2c57f81194f3e75d36cfbffd21a3a7c
JD Gruber <jgruber3@zagmail.gonzaga.edu>
2020-11-28 20:23:33 -0800

    Initial Commit

---
+++ Flick/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flick
+//
+//  Created by Gruber, John David on 11/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
